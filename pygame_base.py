05eabd10cc753830948d4337ea0036ffe518b1c5
Brian <brjhodges@gmail.com>
2016-05-14 14:47:38 -0500

    Base fixes
    Added framerate controller and fixed drawing function.

--- pygame_base.py
+++ pygame_base.py
@@ -5,30 +5,46 @@
 import pygame
 from pygame.locals import *
 
+WHITE = [255, 255, 255]
+BLACK = [  0,   0,   0]
+
 class App:
     def __init__(self):
         self._running = True
-        self._display_surf = None
-        self.width = 640
-        self.height = 400
-        self.size = (self.width, self.height)
+        self.surface = None
+        self.width = 640 # defines window width
+        self.height = 400 # defines window height
+        self.size = [self.width, self.height] # sets the window size
+        self.clock = pygame.time.Clock()
+        self.fps = 60 # sets the game's framerate (i.e. 'ticks' per second)
 
     def on_init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self.surface = pygame.display.set_mode(self.size)
         self._running = True
+
+        # usable variables, this is where you initialize the game's
+        # beginning conditions
+        # should use attributes of self
+        # i.e. self.character for the player's character
 
     def on_event(self, event):
         # things that happen when the user does something
         if event.type == pygame.QUIT:
             self._running = False
-   
+
     def on_loop(self):
-        pass # things that happen every 'tick' of the game
-   
+        # things that happen every 'tick' of the game
+        pass
+
     def on_render(self):
-        pass # the actual drawing of the screen
-   
+        # the actual drawing of the screen
+        self.surface.fill(WHITE)
+        
+        #drawing goes here
+        
+        pygame.display.flip()
+
     def on_cleanup(self):
         pygame.quit()
 
@@ -41,6 +57,7 @@ class App:
                 self.on_event(event)
             self.on_loop()
             self.on_render()
+            self.clock.tick(self.fps)
         self.on_cleanup()
 
 if __name__ == "__main__":
